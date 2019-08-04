c4a0b13913bbbeba17dfab78491c242717e592b3
yyh-gl <yhonda.95.gl@gmail.com>
2019-08-05 00:49:39 +0900

    CORSテスト

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -53,10 +53,6 @@ func corsHandler(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
 	w.Header().Add("Access-Control-Allow-Origin", "http://localhost:1313")
 	w.Header().Set("Access-Control-Allow-Methods", "*")
 	w.Header().Add("Access-Control-Allow-Headers", "Content-Type")
-	w.Header().Add("Access-Control-Allow-Headers", "Origin")
-	w.Header().Add("Access-Control-Allow-Headers", "X-Requested-With")
-	w.Header().Add("Access-Control-Allow-Headers", "Accept")
-	w.Header().Add("Access-Control-Allow-Headers", "Accept-Language")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 }
