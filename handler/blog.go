3520339a38b2746b1ff7af0d0082276d31b94b86
yyh-gl <yhonda.95.gl@gmail.com>
2019-08-04 11:54:07 +0900

    CORSテスト

--- handler/blog.go
+++ handler/blog.go
@@ -74,7 +74,7 @@ func GetBlogHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost")
 	if err := json.NewEncoder(w).Encode(blog); err != nil {
 		logger.Println(err)
 		// TODO: エラーハンドリングをきちんとする
