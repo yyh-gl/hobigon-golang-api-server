0eb7a9c152c1cc98883664702d52814961fbe9dc
yyh-gl <yhonda.95.gl@gmail.com>
2019-09-04 21:27:58 +0900

    現在時刻（JST）を取得する処理を共通化

--- domain/model/task.go
+++ domain/model/task.go
@@ -34,23 +34,20 @@ type TaskList struct {
 }
 
 func (t Task) GetJSTDue(utcDue *time.Time) *time.Time {
-	// TODO: jst 共通化
-	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
+	jst := getJSTNow()
 	jstDue := utcDue.In(jst)
 	return &jstDue
 }
 
 func (t Task) IsDueOver() (isDueOver bool) {
-	// TODO: jst 共通化
-	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
+	jst := getJSTNow()
 	today := time.Now()
 	todayStart := time.Date(today.Year(), today.Month(), today.Day(), 0, 0, 0, 0, jst)
 	return !t.Due.Equal(todayStart) && t.Due.Before(todayStart)
 }
 
 func (t Task) IsTodayTask() (isTodayTask bool) {
-	// TODO: jst 共通化
-	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
+	jst := getJSTNow()
 	today := time.Now()
 	todayStart := time.Date(today.Year(), today.Month(), today.Day(), 0, 0, 0, 0, jst)
 	todayEnd := time.Date(today.Year(), today.Month(), today.Day(), 23, 59, 59, 0, jst)
@@ -67,4 +64,8 @@ func (tl TaskList) GetTodayTasks() (todayTasks []Task) {
 		}
 	}
 	return todayTasks
+}
+
+func getJSTNow() *time.Location {
+	return time.FixedZone("Asia/Tokyo", 9*60*60)
 }
