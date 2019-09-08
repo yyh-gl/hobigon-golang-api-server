2277ff1c9d9974e16795d4da6f7a1242f9e0d448
yyh-gl <yhonda.95.gl@gmail.com>
2019-09-08 10:38:03 +0900

    誕生日通知機能でbodyが不要な形で修正

--- handler/birthday.go
+++ handler/birthday.go
@@ -2,8 +2,8 @@ package handler
 
 import (
 	"encoding/json"
-	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/jinzhu/gorm"
 	"github.com/yyh-gl/hobigon-golang-api-server/app"
@@ -13,35 +13,13 @@ import (
 )
 
 func NotifyBirthdayHandler(w http.ResponseWriter, r *http.Request) {
-	type request struct {
-		Date string `json:"date"`
-	}
-
 	logger := app.Logger
 
 	birthdayRepository := repository.NewBirthdayRepository()
 	slackGateway := gateway.NewSlackGateway()
 
-	// TODO: body から受け取る必要なくない？
-	defer r.Body.Close()
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		logger.Println(err)
-		// TODO: エラーハンドリングをきちんとする
-		http.Error(w, "Internal Server Error", 500)
-		return
-	}
-
-	var req request
-	err = json.Unmarshal(body, &req)
-	if err != nil {
-		logger.Println(err)
-		// TODO: エラーハンドリングをきちんとする
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
-	birthday, err := birthdayRepository.SelectByDate(req.Date)
+	today := time.Now().Format("0102")
+	birthday, err := birthdayRepository.SelectByDate(today)
 	if err != nil && err != gorm.ErrRecordNotFound {
 		logger.Println(err)
 		// TODO: エラーハンドリングをきちんとする
