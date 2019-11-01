759f986db98348c66710723d134f9cf8da766a68
yyh-gl <yhonda.95.gl@gmail.com>
2019-11-02 00:59:02 +0900

    誕生日に関して依存関係の定義をmain.goでやるように修正

--- infra/repository/birthday.go
+++ infra/repository/birthday.go
@@ -7,16 +7,26 @@ import (
 	"github.com/yyh-gl/hobigon-golang-api-server/domain/repository"
 )
 
+//////////////////////////////////////////////////
+// NewBirthdayRepository
+//////////////////////////////////////////////////
+
 type birthdayRepository struct {
 	db *gorm.DB
 }
 
+// NewBirthdayRepository : 誕生日用のリポジトリを取得
 func NewBirthdayRepository() repository.BirthdayRepository {
 	return &birthdayRepository{
 		db: app.DB,
 	}
 }
 
+//////////////////////////////////////////////////
+// SelectByDate
+//////////////////////////////////////////////////
+
+// SelectByDate : 日付から誕生日を1件取得
 func (br birthdayRepository) SelectByDate(date string) (birthday model.Birthday, err error) {
 	err = br.db.First(&birthday, "date=?", date).Error
 	if err != nil {
