552bb06ff10476f7fc0de9632b45cdba623895f1
yyh-gl <yhonda.95.gl@gmail.com>
2019-11-03 12:59:51 +0900

    Birthdayモデルのコンストラクタを作成

--- domain/model/birthday.go
+++ domain/model/birthday.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"errors"
+	"strings"
 	"time"
 )
 
@@ -15,11 +17,38 @@ type Birthday struct {
 	DeletedAt *time.Time
 }
 
+func NewBirthday(
+	id uint,
+	name string,
+	date time.Time,
+	wishList string,
+) (*Birthday, error) {
+	// WishList のバリデーション
+	//  -> WishList が空じゃない場合は "https://" で始まっていることをチェック
+	if wishList != "" && !strings.HasPrefix(wishList, "https://") {
+		return nil, errors.New("バリデーションエラー：【Birthday】WishList が \"https://\" から始まっていません")
+	}
+
+	return &Birthday{
+		ID:       id,
+		Name:     name,
+		Date:     convertStringDate(date),
+		WishList: wishList,
+	}, nil
+}
+
+// time.Time 形式の日付を文字列形式（MMdd）に変換
+func convertStringDate(date time.Time) string {
+	return date.Format("0102")
+}
+
+// 指定 Birthday が本日のものかどうか判定
 func (b Birthday) IsToday() bool {
-	today := time.Now().Format("0102")
+	today := convertStringDate(time.Now())
 	return b.Date == today
 }
 
+// 誕生日メッセージを生成
 func (b Birthday) CreateBirthdayMessage() string {
 	if b.WishList == "" {
 		b.WishList = "Amazon の欲しい物リスト教えて！"
