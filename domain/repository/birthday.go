c13590b3372a858ec67c300519135cc259b03470
yyh-gl <yhonda.95.gl@gmail.com>
2019-11-03 14:06:48 +0900

    コメント追加

--- domain/repository/birthday.go
+++ domain/repository/birthday.go
@@ -6,6 +6,7 @@ import (
 	"github.com/yyh-gl/hobigon-golang-api-server/domain/model"
 )
 
+// BirthdayRepository : 誕生日用のリポジトリインターフェース
 type BirthdayRepository interface {
 	Create(ctx context.Context, birthday model.Birthday) (*model.Birthday, error)
 	SelectByDate(ctx context.Context, date string) (*model.Birthday, error)
