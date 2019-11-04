85c84662cf33d24a065e9e33afe896128466e187
yyh-gl <yhonda.95.gl@gmail.com>
2019-11-04 10:52:53 +0900

    コメント追加

--- infra/imodel/birthday.go
+++ infra/imodel/birthday.go
@@ -8,6 +8,7 @@ import (
 	"github.com/yyh-gl/hobigon-golang-api-server/domain/model/entity"
 )
 
+// BirthdayDTO : 誕生日用の DTO
 type BirthdayDTO struct {
 	ID        uint   `gorm:"primary_key;AUTO_INCREMENT"`
 	Name      string `gorm:"name;not null"`
@@ -18,10 +19,12 @@ type BirthdayDTO struct {
 	DeletedAt *time.Time
 }
 
+// TableName : DB アクセスにおける対応テーブル名
 func (b BirthdayDTO) TableName() string {
 	return "birthdays"
 }
 
+// ConvertToDomainModel : ドメインモデルに変換
 func (b BirthdayDTO) ConvertToDomainModel() (*entity.Birthday, error) {
 	// time.Time 型の日付情報を取得
 	month, err := strconv.Atoi(b.Date[0:2])
