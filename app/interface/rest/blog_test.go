a2b91cafdfb582539101bacf63a36d666a3a1ad0
yyh-gl <yhonda.95.gl@gmail.com>
2020-01-20 23:48:43 +0900

    TestBlogHandler_Show実装

--- app/interface/rest/blog_test.go
+++ app/interface/rest/blog_test.go
@@ -2,6 +2,7 @@ package rest_test
 
 import (
 	"encoding/json"
+	"net/http"
 	"testing"
 
 	"github.com/bmizerany/assert"
@@ -16,17 +17,19 @@ type blogResponse struct {
 }
 
 func createBlog(c *test.Client, title string) {
-	body := `
-{
+	c.AddRoute(http.MethodPost, "/api/v1/blogs", c.DIContainer.HandlerBlog.Create)
+
+	body := `{
   "title": "` + title + `"
-}
-`
-	_ = c.Post(c.DIContainer.HandlerBlog.Create, "/api/v1/blogs", body)
+}`
+	_ = c.Post("/api/v1/blogs", body)
 }
 
 func TestBlogHandler_Create(t *testing.T) {
 	c := test.NewClient()
 	defer func() { _ = c.DIContainer.DB.Close() }()
+
+	c.AddRoute(http.MethodPost, "/api/v1/blogs", c.DIContainer.HandlerBlog.Create)
 
 	testCases := []struct {
 		title string
@@ -57,8 +60,7 @@ func TestBlogHandler_Create(t *testing.T) {
 		body := `{
   "title": "` + tc.title + `"
 }`
-
-		rec := c.Post(c.DIContainer.HandlerBlog.Create, "/api/v1/blogs", body)
+		rec := c.Post("/api/v1/blogs", body)
 		resp := new(blogResponse)
 		_ = json.Unmarshal(rec.Body.Bytes(), &resp)
 
@@ -68,53 +70,39 @@ func TestBlogHandler_Create(t *testing.T) {
 }
 
 func TestBlogHandler_Show(t *testing.T) {
-	//app.Init("")
-	//
-	//blogRepository := irepository.NewBlogRepository()
-	//slackGateway := igateway.NewSlackGateway()
-	//blogUseCase := usecase.NewBlogUseCase(blogRepository, slackGateway)
-	//blogHandler := rest.NewBlogHandler(blogUseCase)
-	//s := httptest.NewServer(http.HandlerFunc(blogHandler.Show))
-	//defer s.Close()
-	//
-	//res, _ := http.Get(s.URL + "/")
-	//
-	//defer res.Body.Close()
-	//body, _ := ioutil.ReadAll(res.Body)
-	//fmt.Println("========================")
-	//fmt.Println(body)
-	//fmt.Println("========================")
-	//testCases := []struct {
-	//	title string
-	//	want  string
-	//	err   error
-	//}{
-	//	{ // 正常系
-	//		title: "sample-blog-title",
-	//		want:  "sample-blog-title",
-	//		err:   nil,
-	//	},
-	//	{ // 正常系：存在しないタイトル
-	//		title: "no-exist-sample-blog-title",
-	//		want:  "",
-	//		// TODO: err を返さないようにする
-	//		err: errors.New("blogRepository.SelectByTitle()内でのエラー: gorm.First(blog)内でのエラー: record not found"),
-	//	},
-	//}
-	//for _, tc := range testCases {
-	//	url := fmt.Sprintf("http://localhost:3000/api/v1/blogs/%s", tc.title)
-	//	resp, _ := http.Get(url)
-	//	defer resp.Body.Close()
-	//	byteArray, _ := ioutil.ReadAll(resp.Body)
-	//
-	//	res := new(blogResponse)
-	//	_ = json.Unmarshal(byteArray, &res)
-	//
-	//	assert.Equal(t, tc.want, res.Blog.Title)
-	//	if res.Error != "" {
-	//		assert.Equal(t, tc.err.Error(), res.Error)
-	//	}
-	//}
+	c := test.NewClient()
+	defer func() { _ = c.DIContainer.DB.Close() }()
+
+	c.AddRoute(http.MethodGet, "/api/v1/blogs/:title", c.DIContainer.HandlerBlog.Show)
+
+	testCases := []struct {
+		title string
+		want  string
+		err   string
+	}{
+		{ // 正常系
+			title: "sample-blog-title",
+			want:  "sample-blog-title",
+			err:   "",
+		},
+		{ // 異常系：タイトルを渡さない
+			title: "",
+			want:  "",
+			err:   "",
+		},
+	}
+
+	// テストデータを追加
+	createBlog(c, "sample-blog-title")
+
+	for _, tc := range testCases {
+		rec := c.Get(c.DIContainer.HandlerBlog.Show, "/api/v1/blogs/"+tc.title)
+		resp := new(blogResponse)
+		_ = json.Unmarshal(rec.Body.Bytes(), &resp)
+
+		assert.Equal(t, tc.want, resp.Blog.Title())
+		assert.Equal(t, tc.err, resp.Error)
+	}
 }
 
 func TestBlogHandler_Like(t *testing.T) {
