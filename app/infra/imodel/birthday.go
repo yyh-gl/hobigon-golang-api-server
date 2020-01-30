73d0d5bc8ee4b91306306392abd6f59e6aa5aee8
yyh-gl <yhonda.95.gl@gmail.com>
2020-01-31 00:36:42 +0900

    コンバーター修正

--- app/infra/imodel/birthday.go
+++ app/infra/imodel/birthday.go
@@ -3,7 +3,6 @@ package imodel
 import (
 	"context"
 	"fmt"
-	"strconv"
 	"time"
 
 	"github.com/yyh-gl/hobigon-golang-api-server/app/domain/model/birthday"
@@ -27,20 +26,13 @@ func (b BirthdayDTO) TableName() string {
 
 // ConvertToDomainModel : ドメインモデルに変換
 func (b BirthdayDTO) ConvertToDomainModel(ctx context.Context) (*birthday.Birthday, error) {
-	// time.Time 型の日付情報を取得
-	month, err := strconv.Atoi(b.Date[0:2])
-	if err != nil {
-		return nil, fmt.Errorf("hour取得におけるstrconv.Atoi()内でのエラー: %w", err)
-	}
-	day, err := strconv.Atoi(b.Date[2:4])
-	if err != nil {
-		return nil, fmt.Errorf("hour取得におけるstrconv.Atoi()内でのエラー: %w", err)
-	}
-	date := time.Date(0, time.Month(month), day, 0, 0, 0, 0, time.Local)
-
 	// Birthday モデルを取得
 	domainModelBirthday, err := birthday.NewBirthdayWithFullParams(
-		b.ID, b.Name, date, b.WishList, b.CreatedAt, b.UpdatedAt, b.DeletedAt,
+		b.ID, b.Name, b.Date, b.WishList, b.CreatedAt, b.UpdatedAt, b.DeletedAt,
 	)
+	if err != nil {
+		return nil, fmt.Errorf("birthday.NewBirthdayWithFullParams()でエラー: %w", err)
+	}
+
 	return domainModelBirthday, nil
 }
