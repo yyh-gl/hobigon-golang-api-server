b40d20b1fb7604e36d1e547f0bc8931d7ce3aaac
yyh-gl <yhonda.95.gl@gmail.com>
2020-07-23 17:41:50 +0900

    Add comments

--- app/interface/rest/error.go
+++ app/interface/rest/error.go
@@ -1,5 +1,6 @@
 package rest
 
+// errorResponse : API用の共通エラーレスポンス構造体
 type errorResponse struct {
 	Error errResp `json:"error"`
 }
@@ -9,11 +10,14 @@ type errResp struct {
 }
 
 var (
+	// errBadRequest : 400 Bad Request
 	errBadRequest = errorResponse{
 		Error: errResp{
 			Detail: "不正なリクエスト形式です",
 		},
 	}
+
+	// errInterServerError : 500 Internal Server Error
 	errInterServerError = errorResponse{
 		Error: errResp{
 			Detail: "サーバ内でエラーが発生しました",
