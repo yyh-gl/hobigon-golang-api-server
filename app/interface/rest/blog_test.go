6c09d229075eaf019a9c09ec0d9b8c6127908b1d
yyh-gl <yhonda.95.gl@gmail.com>
2020-07-23 16:32:03 +0900

    Fix test

--- app/interface/rest/blog_test.go
+++ app/interface/rest/blog_test.go
@@ -9,70 +9,66 @@ import (
 	"github.com/yyh-gl/hobigon-golang-api-server/test"
 )
 
-//func createBlog(c *test.Client, title string) {
-//	c.AddRoute(http.MethodPost, "/api/v1/blogs", c.DIContainer.HandlerBlog.Create)
-//
-//	body := `{
-//  "title": "` + title + `"
-//}`
-//	_ = c.Post("/api/v1/blogs", body)
-//}
-
 //func TestBlogHandler_Create(t *testing.T) {
-//	c := test.NewClient()
-//	defer func() { _ = c.DIContainer.DB.Close() }()
+//	type want struct {
+//		body       string
+//		statusCode int
+//	}
 //
-//	c.AddRoute(http.MethodPost, "/api/v1/blogs", c.DIContainer.HandlerBlog.Create)
-//
-//	testCases := []struct {
+//	tests := []struct {
 //		title string
-//		want  string
+//		want  want
 //		err   string
 //	}{
 //		{ // 正常系
 //			title: "sample-blog-title",
-//			want:  "sample-blog-title",
-//			err:   "",
+//			want: want{
+//				body:       `{"Title":"sample-blog-title","Count":0}`,
+//				statusCode: http.StatusCreated,
+//			},
+//			err: "",
 //		},
-//		{ // 正常系：50文字タイトル
-//			title: "hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-title",
-//			want:  "hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-title",
-//			err:   "",
-//		},
-//		{ // 異常系：51文字タイトル
-//			title: "hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-title-over",
-//			want:  "",
-//			err:   "bindReqWithValidate()でエラー: バリデーションエラー: Key: 'request.Title' Error:Field validation for 'Title' failed on the 'max' tag",
-//		},
-//		{ // 異常系：タイトルを渡さない
-//			title: "",
-//			want:  "",
-//			err:   "bindReqWithValidate()でエラー: バリデーションエラー: Key: 'request.Title' Error:Field validation for 'Title' failed on the 'required' tag",
-//		},
-//		{ // 異常系：duplicate
-//			title: "duplicate-blog-title",
-//			want:  "",
-//			err:   "BlogUseCase.Create()でエラー: blogRepository.Create()内でのエラー: gorm.Create(blog)内でのエラー: UNIQUE constraint failed: blog_posts.title",
-//		},
+//		//{ // 正常系：50文字タイトル
+//		//	title: "hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-title",
+//		//	want:  "hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-title",
+//		//	err:   "",
+//		//},
+//		//{ // 異常系：51文字タイトル
+//		//	title: "hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-title-over",
+//		//	want:  "",
+//		//	err:   "bindReqWithValidate()でエラー: バリデーションエラー: Key: 'request.Title' Error:Field validation for 'Title' failed on the 'max' tag",
+//		//},
+//		//{ // 異常系：タイトルを渡さない
+//		//	title: "",
+//		//	want:  "",
+//		//	err:   "bindReqWithValidate()でエラー: バリデーションエラー: Key: 'request.Title' Error:Field validation for 'Title' failed on the 'required' tag",
+//		//},
+//		//{ // 異常系：duplicate
+//		//	title: "duplicate-blog-title",
+//		//	want:  "",
+//		//	err:   "BlogUseCase.Create()でエラー: blogRepository.Create()内でのエラー: gorm.Create(blog)内でのエラー: UNIQUE constraint failed: blog_posts.title",
+//		//},
 //	}
 //
 //	// 重複データ登録時に使用するテストデータを追加
-//	createBlog(c, "duplicate-blog-title")
+//	test.CreateBlog(DIContainer.DB, "duplicate-blog-title")
+//	defer test.DeleteBlog(DIContainer.DB, "duplicate-blog-title")
 //
-//	for _, tc := range testCases {
-//		body := `{
-// "title": "` + tc.title + `"
-//}`
-//		rec := c.Post("/api/v1/blogs", body)
-//		resp := rest.BlogResponse{}
-//		_ = json.Unmarshal(rec.Body.Bytes(), &resp)
+//	for _, tt := range tests {
+//		reqBody := strings.NewReader(`{"title":"` + tt.title + `"}`)
+//		req, _ := http.NewRequest(http.MethodPost, "/api/v1/blogs", reqBody)
+//		rr := httptest.NewRecorder()
+//		Router.ServeHTTP(rr, req)
 //
-//		if tc.err == "" {
-//			assert.Equal(t, tc.want, resp.Blog.Title().String())
-//			assert.Equal(t, "", resp.Error)
-//		} else {
-//			assert.Equal(t, (*blog.Blog)(nil), resp.Blog)
-//			assert.Equal(t, tc.err, resp.Error)
+//		if c := rr.Code; c != tt.want.statusCode {
+//			t.Errorf("handler returned wrong status code: got %v want %v",
+//				c, tt.want.statusCode)
+//		}
+//
+//		respBody := strings.TrimRight(rr.Body.String(), "\n")
+//		if respBody != tt.want.body {
+//			t.Errorf("handler returned unexpected body: got %v want %v",
+//				respBody, tt.want.body)
 //		}
 //	}
 //}
@@ -91,7 +87,7 @@ func TestBlogHandler_Show(t *testing.T) {
 		{ // 正常系
 			title: "sample-blog-title",
 			want: want{
-				body:       `{"Title":"sample-blog-title","Count":0}`,
+				body:       `{"title":"sample-blog-title","count":0}`,
 				statusCode: http.StatusOK,
 			},
 			err: "",
@@ -107,7 +103,7 @@ func TestBlogHandler_Show(t *testing.T) {
 		{ // 正常系：50文字タイトル
 			title: "hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-title",
 			want: want{
-				body:       `{"Title":"hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-title","Count":0}`,
+				body:       `{"title":"hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-title","count":0}`,
 				statusCode: http.StatusOK,
 			},
 			err: "",
@@ -154,34 +150,34 @@ func TestBlogHandler_Show(t *testing.T) {
 //
 //	c.AddRoute(http.MethodPost, "/api/v1/blogs/:title/like", c.DIContainer.HandlerBlog.Like)
 //
-//	testCases := []struct {
+//	testtases := []struct {
 //		title     string
 //		wantTitle string
-//		wantCount int
+//		wanttount int
 //		err       string
 //	}{
 //		{ // 正常系
 //			title:     "sample-blog-title",
 //			wantTitle: "sample-blog-title",
-//			wantCount: 1,
+//			wanttount: 1,
 //			err:       "",
 //		},
 //		{ // 正常系：存在しないブログ
 //			title:     "sample-blog-title2",
 //			wantTitle: "",
-//			wantCount: 0,
+//			wanttount: 0,
 //			err:       "",
 //		},
 //		{ // 正常系：50文字タイトル
 //			title:     "hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-title",
 //			wantTitle: "hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-title",
-//			wantCount: 1,
+//			wanttount: 1,
 //			err:       "",
 //		},
 //		{ // 異常系：51文字タイトル
 //			title:     "hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-title-over",
 //			wantTitle: "",
-//			wantCount: 0,
+//			wanttount: 0,
 //			// TODO: ドメインモデル用テストを作って、NewTitle()におけるバリデーションが動作するか確認
 //			err: "bindReqWithValidate()でエラー: バリデーションエラー: Key: 'request.Title' Error:Field validation for 'Title' failed on the 'max' tag",
 //		},
@@ -191,22 +187,22 @@ func TestBlogHandler_Show(t *testing.T) {
 //	createBlog(c, "sample-blog-title")
 //	createBlog(c, "hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-hoge-title")
 //
-//	for _, tc := range testCases {
-//		rec := c.Post("/api/v1/blogs/"+tc.title+"/like", "")
+//	for _, tt := range testtases {
+//		rec := c.Post("/api/v1/blogs/"+tt.title+"/like", "")
 //		resp := rest.BlogResponse{}
 //		_ = json.Unmarshal(rec.Body.Bytes(), &resp)
 //
-//		if tc.err == "" {
-//			if tc.wantTitle != "" {
-//				assert.Equal(t, tc.wantTitle, resp.Blog.Title().String())
+//		if tt.err == "" {
+//			if tt.wantTitle != "" {
+//				assert.Equal(t, tt.wantTitle, resp.Blog.Title().String())
 //			}
-//			if tc.wantCount != 0 {
-//				assert.Equal(t, tc.wantCount, resp.Blog.Count().Int())
+//			if tt.wanttount != 0 {
+//				assert.Equal(t, tt.wanttount, resp.Blog.Count().Int())
 //			}
 //			assert.Equal(t, "", resp.Error)
 //		} else {
 //			assert.Equal(t, (*blog.Blog)(nil), resp.Blog)
-//			assert.Equal(t, tc.err, resp.Error)
+//			assert.Equal(t, tt.err, resp.Error)
 //		}
 //	}
 //}
