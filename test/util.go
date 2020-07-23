521b167d99204193338712196d9b4165d684f489
yyh-gl <yhonda.95.gl@gmail.com>
2020-07-23 15:59:24 +0900

    テスト用Util追加

---
+++ test/util.go
@@ -0,0 +1,19 @@
+package test
+
+import (
+	"github.com/yyh-gl/hobigon-golang-api-server/app/infra/db"
+	"github.com/yyh-gl/hobigon-golang-api-server/app/infra/dto"
+)
+
+func CreateBlog(db *db.DB, title string) {
+	_ = db.Create(&dto.BlogDTO{
+		Title: title,
+		Count: 0,
+	}).Error
+}
+
+func DeleteBlog(db *db.DB, title string) {
+	_ = db.Delete(&dto.BlogDTO{
+		Title: title,
+	}).Error
+}
